db3b9d308d4344712e088831cecb42e29688bf99
JunJune0615 <168808114+JunJune0615@users.noreply.github.com>
2024-06-12 20:40:58 +0900

    課題020を提出

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const changeBtn = document.getElementById('btn');
+
+changeBtn.addEventListener('click', () =>{
+    const changeWord = document.getElementById('text');
+    changeWord.textContent = 'ボタンをクリックしました'
+});
